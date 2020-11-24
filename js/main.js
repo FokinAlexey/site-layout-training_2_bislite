566028325ffc1ce1b532707ae9bf2ecc6c41be75
FokinAlexey <a.fokin1979@gmail.com>
2020-11-24 17:21:50 +0300

    fix: fin testing

--- js/main.js
+++ js/main.js
@@ -10,20 +10,46 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
-
+        responsive: [
+            {
+              breakpoint: 911,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 681,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                }
+              },
+              {
+                breakpoint: 455,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+    
+          ]
     });
+
+
+    $('.header__menu-btn').on('click', function(){
+        $('.header__menu > ul').slideToggle(); /*slideToggle-делает всплывающее меню*/
+    })
+
+    $('.header__drop-down.drop-down').on('click', function(){
+        $(this).children('.drop-down__list').toggleClass('active'); 
+        $(this).children('.drop-down__link').toggleClass('active');
+    })
+
+
 
 });
           
-  
 
 
-//   $(document).ready(function(){
-    
-//     $('.slider__inner').slick({
-        
-//     });
 
-
-//   });
-       
